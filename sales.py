f23111f6bc5297e912899237aca7fc2f9f35abfa
yasaman <yasamanabbasi92@gmail.com>
2026-08-19 22:03:47 +0330

    improve sales.py

--- sales.py
+++ sales.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-num = int(input("How many item do you have? "))
+while True:
+    try:
+        num = int(input("How many item do you have? "))
+        break
+    except ValueError:
+        print("number please")
+
 sales = []
 
 for i in range(num):
@@ -19,3 +25,33 @@ print("Highest sale: ", df.loc[maxi, "product"], "-", df.loc[maxi, "price"])
 print("Lowest sale: ", df.loc[mini, "product"], "-", df.loc[mini, "price"])
 print(df.groupby("product")["price"].count())
 print(df.groupby("product")["price"].sum())
+
+prd_list = list(df["product"])
+
+found = False
+while found == False:
+    selected = input("which product do you want to analyze? ").strip().lower()
+    for j in range(len(prd_list)):
+        if  (prd_list[j] == selected):
+            found = True
+            break;
+
+df["selected"] = (df["product"] == selected)
+
+selected_df = df.loc[df["selected"]==True]
+print("Product: ", selected)
+print("Number of sales: ", selected_df["price"].count())
+print("Total revenue: ", selected_df["price"].sum())
+print("Average price: ", selected_df["price"].mean())
+
+cdf = pd.Series(df.groupby("product")["price"].count(), name="Count")
+sdf = pd.Series(df.groupby("product")["price"].sum(), name="Sum")
+mdf = pd.Series(df.groupby("product")["price"].mean(), name="Mean")
+
+count_df = pd.DataFrame(cdf)
+sum_df = pd.DataFrame(sdf)
+mean_df = pd.DataFrame(mdf)
+
+frames = [count_df, sum_df, mean_df]
+table = pd.concat(frames, axis=1)
+print(table)
